5dd97a3e7893d993b7b24a522592a4416e8b687e
shibamoto <silverbirder@gmail.com>
2020-10-18 11:41:49 +0900

    🛠 Fixed "Failed to execute function: parameter 1 is not of type 'Response'."

--- packages/team-search-fragment-workers/workers-site/index.js
+++ packages/team-search-fragment-workers/workers-site/index.js
@@ -8,20 +8,18 @@ async function handleEvent(event) {
     const url = new URL(event.request.url);
     const pathname = url.pathname;
     try {
-        switch (pathname) {
-            case '/manifest.json':
-                const json = JSON.stringify({
-                    'html': `/index.html`,
-                    'js': `/bundle.js`,
-                }, null, 2);
-                return new Response(json, {
-                    headers: {
-                        "content-type": "application/json;charset=UTF-8"
-                    }
-                });
-            case '/bundle.js':
-            case '/index.html':
-                return await getAssetFromKV(event);
+        if (pathname === '/manifest.json') {
+            const json = JSON.stringify({
+                html: `/index.html`,
+                js: `/bundle.js`
+            });
+            return new Response(json, {
+                headers: {
+                    "content-type": "application/json;charset=UTF-8"
+                }
+            });
+        } else {
+            return await getAssetFromKV(event);
         }
     } catch (e) {
         return new Response(`"${pathname}" not found`, {

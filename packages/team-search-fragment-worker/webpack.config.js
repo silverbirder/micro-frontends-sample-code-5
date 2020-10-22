65f473e41afa51fc0a7aeda5681cf7509aec9e70
shibamoto <silverbirder@gmail.com>
2020-10-23 07:28:20 +0900

    🛠 Refactoring webpack.config.js

--- packages/team-search-fragment-worker/webpack.config.js
+++ packages/team-search-fragment-worker/webpack.config.js
@@ -5,63 +5,43 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const mode = process.env.NODE_ENV || 'production'
 
 module.exports = {
-  entry: './src/client.ts',
-  output: {
-    filename: `worker.${mode}.js`,
-    path: path.join(__dirname, 'dist'),
-  },
-  mode,
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    plugins: [],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true,
-        },
-      },
-    ],
-  },
-  entry: './src/client.ts',
-  output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [["@babel/preset-env", {
-                useBuiltIns: "usage",
-                corejs: 3
-              }]],
-              plugins: [
-                "@babel/proposal-class-properties",
-                "@babel/proposal-object-rest-spread"
-              ]
-            }
-          },
-          {loader: 'ts-loader'}
+    mode,
+    entry: './src/client.ts',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [["@babel/preset-env", {
+                                useBuiltIns: "usage",
+                                corejs: 3
+                            }]],
+                            plugins: [
+                                "@babel/proposal-class-properties",
+                                "@babel/proposal-object-rest-spread"
+                            ]
+                        }
+                    },
+                    {loader: 'ts-loader'}
+                ],
+                exclude: /node_modules/,
+            },
         ],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: false,
-    }),
-  ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: mode !== 'production',
+        }),
+    ]
 }
